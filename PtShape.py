61bb00e2e63e6813fda0530c95f44e26d2ec5fba
Chris Elston <chris.elston@blueyonder.co.uk>
2022-04-13 23:53:09 +0100

    remove debug message

--- PtShape.py
+++ PtShape.py
@@ -15,7 +15,6 @@ class PtShape:
         self.posx = self.posy = 0
 
     def move(self, movement):
-        print(movement)
         if(movement == MV_LEFT):
             self.posx -= 1
         elif(movement == MV_RIGHT):
