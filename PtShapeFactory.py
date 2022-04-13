86fee1134faa1698016ab57f472b48fab0925540
Chris Elston <chris.elston@blueyonder.co.uk>
2022-04-13 16:56:16 +0100

    PtShapeFactory: remove numpy dependency
    explicitly hard-code all shape variants

--- PtShapeFactory.py
+++ PtShapeFactory.py
@@ -1,86 +1,153 @@
 from PtShape import PtShape
 from functools import reduce
 import random
-import numpy as np
 
 class PtShapeFactory:
     """store all possible shape formations/rotations and 
        deliver new PtShape objects to the game when requested"""
 
     def __init__(self):
-        """store all possible _base_forms in an array
-           each within different sized squares for a reasonable
-           centre of rotation"""
+        """store four rotational variants for each shape
+           designed so the bottom row always represents
+           the bottom of the shape"""
 
         # first define all the basic forms
-        self._base_forms = []
+        self._shapes = []
 
-        self._base_forms.append([[1,1],
-                                 [1,1]])
+        shape0 = [[1,1],
+                  [1,1]]
 
-        self._base_forms.append([[0,0,0],
-                                 [1,1,1],
-                                 [0,1,0]])
+        self._shapes.append([shape0, shape0, shape0, shape0])
 
-        self._base_forms.append([[0,0,0],
-                                 [1,1,0],
-                                 [0,1,1]])
+        shape1 = [[0,0,0],
+                  [0,0,0],
+                  [0,0,0]]
 
-        self._base_forms.append([[0,0,0],
-                                 [0,1,1],
-                                 [1,1,0]])
+        shape0 = [[0,0,0],
+                  [1,1,1],
+                  [0,1,0]]
 
-        self._base_forms.append([[0,0,0,0],
-                                 [1,1,1,1],
-                                 [0,0,0,0],
-                                 [0,0,0,0]])
+        shape1 = [[0,1,0],
+                  [1,1,0],
+                  [0,1,0]]
 
-        self._base_forms.append([[0,0,0,0],
-                                 [0,1,1,0],
-                                 [0,1,0,0],
-                                 [0,1,0,0]])
+        shape2 = [[0,0,0],
+                  [0,1,0],
+                  [1,1,1]]
 
-        self._base_forms.append([[0,0,0,0],
-                                 [0,1,1,0],
-                                 [0,1,0,0],
-                                 [0,1,0,0]])
+        shape3 = [[0,1,0],
+                  [0,1,1],
+                  [0,1,0]]
 
-        self._shape_variants = []
+        self._shapes.append([shape0, shape1, shape2, shape3])
 
-        # now create a list of all rotational shape variants
-        # increment rotation to rotate by 90deg clockwise
-        for form in self._base_forms:
-            variants = []
-            for i in range(4):
-                #rotate the form
-                variant = np.rot90(form, i).tolist()
+        shape0 = [[0,0,0],
+                  [1,1,0],
+                  [0,1,1]]
 
-                # remove any empty rows
-                for j in reversed(range(len(variant))):
-                    if not any(variant[j]):
-                        del variant[j]
+        shape1 = [[0,1,0],
+                  [1,1,0],
+                  [1,0,0]]
 
-                variants.append(variant)
+        shape3 = [[0,0,1],
+                  [0,1,1],
+                  [0,1,0]]
 
-            self._shape_variants.append(variants)
+        self._shapes.append([shape0, shape1, shape0, shape3])
+
+        shape0 = [[0,0,0],
+                  [0,1,1],
+                  [1,1,0]]
+
+        shape1 = [[1,0,0],
+                  [1,1,0],
+                  [0,1,0]]
+
+        shape3 = [[0,1,0],
+                  [0,1,1],
+                  [0,0,1]]
+
+        self._shapes.append([shape0, shape1, shape0, shape3])
+
+        shape0 = [[0,0,0,0],
+                  [0,0,0,0],
+                  [0,0,0,0],
+                  [1,1,1,1]]
+
+        shape1 = [[0,1,0,0],
+                  [0,1,0,0],
+                  [0,1,0,0],
+                  [0,1,0,0]]
+
+        shape3 = [[0,0,1,0],
+                  [0,0,1,0],
+                  [0,0,1,0],
+                  [0,0,1,0]]
+
+        self._shapes.append([shape0, shape1, shape0, shape3])
+
+        shape0 = [[0,0,0,0],
+                  [0,1,0,0],
+                  [0,1,0,0],
+                  [0,1,1,0]]
+
+        shape1 = [[0,0,0,0],
+                  [0,0,0,0],
+                  [0,1,1,1],
+                  [0,1,0,0]]
+
+        shape2 = [[0,0,0,0],
+                  [0,1,1,0],
+                  [0,0,1,0],
+                  [0,0,1,0]]
+
+        shape3 = [[0,0,0,0],
+                  [0,0,0,0],
+                  [0,0,1,0],
+                  [1,1,1,0]]
+
+        self._shapes.append([shape0, shape1, shape2, shape3])
+
+        shape0 = [[0,0,0,0],
+                  [0,0,1,0],
+                  [0,0,1,0],
+                  [0,1,1,0]]
+
+        shape1 = [[0,0,0,0],
+                  [0,0,0,0],
+                  [0,1,0,0],
+                  [0,1,1,1]]
+
+        shape2 = [[0,0,0,0],
+                  [0,1,1,0],
+                  [0,1,0,0],
+                  [0,1,0,0]]
+
+        shape3 = [[0,0,0,0],
+                  [0,0,0,0],
+                  [1,1,1,0],
+                  [0,0,1,0]]
+
+        self._shapes.append([shape0, shape1, shape2, shape3])
+
+        self._num_shapes = len(self._shapes)
 
         # initialise the form history
         # avoid the "S" and "Z" shapes to start
-        self._form_hist = [2,3,2,3]
+        self._shape_hist = [2,3,2,3]
 
     def new_shape(self):
         """return a pseudo-random shape to the caller
            4 piece history with 4 rolls
            see https://simon.lc/the-history-of-tetris-randomizers"""
-        form = 0
+        shape_no = 0
         for i in range(4):
             # four attempts to choose a shape that doesn't appear in the history
             # if the first three attempts fail always choose the fourth shape
-            form = random.randrange(len(self._base_forms))
-            if form not in self._form_hist: break
-        del self._form_hist[0]
-        self._form_hist.append(form)
+            shape_no = random.randrange(self._num_shapes)
+            if shape_no not in self._shape_hist: break
+        del self._shape_hist[0]
+        self._shape_hist.append(shape_no)
 
-        shape =  PtShape(self._shape_variants[form])
-        return shape
+        return PtShape(self._shapes[shape_no])
 
